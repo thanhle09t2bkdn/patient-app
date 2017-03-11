5497438ace84c98173d037070c090180e0fb8e5b
sonvu <ngocson.bkdn@gmail.com>
2017-03-11 14:55:31 +0700

    api

--- server/controllers/PatientController.js
+++ server/controllers/PatientController.js
@@ -1,5 +1,36 @@
+const Patient = require('../models').Patient;
+const httpStatus = require('http-status');
+const Util = require('../helpers/Util');
+
 module.exports = {
-	index(req, res){
-		res.json("Load patient OK");
+	index(req, res) {
+		Patient.findAll().then((patients) => {
+			return res.json(Util.success('Get patients success!', patients));
+		})
+		.catch((error) => res.status(httpStatus.BAD_REQUEST).json(Util.error(error.message, httpStatus.BAD_REQUEST)));
+	},
+	update(req, res) {
+		const id = req.params.id;
+		let newPatient = {
+			name: req.body.name,
+			gender: req.body.gender,
+			avatar: req.body.avatar,
+			birthday: req.body.birthday,
+			pastMediacation: req.body.pastMediacation,
+			tags: req.body.tags,
+			contact: req.body.contact,
+			pregnancy: req.body.pregnancy,
+			elaboration: req.body.elaboration,
+		};
+		Patient.update(newPatient, {
+		  where: {
+			id: id
+		  }
+		})
+		.then((updatedPatient) => {
+			console.log(updatedPatient);
+			return res.json(Util.success('Update patients success!', updatedPatient));
+		})
+		.catch((error) => res.status(httpStatus.BAD_REQUEST).json(Util.error(error.message, httpStatus.BAD_REQUEST)));
 	}
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const AuthController = require('../controllers').AuthController;
 const PatientController = require('../controllers').PatientController;
+const UploadController = require('../controllers').UploadController;
 const AuthAPI = require('../middlewares/AuthAPI');
 const isAdmin = require('../middlewares/isAdmin');
 module.exports = (app) => {
@@ -8,4 +9,6 @@ module.exports = (app) => {
   }));
   app.post('/api/login', AuthController.login);
   app.get('/api/patient/index', [AuthAPI, isAdmin], PatientController.index);
+  app.put('/api/patient/update/:id', [AuthAPI, isAdmin], PatientController.update);
+  app.post('/api/upload', UploadController.index);
 };
