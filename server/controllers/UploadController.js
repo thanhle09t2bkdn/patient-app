321fbc5e715a32722b608cda725978afbc3a6323
thanhle <thanhle09t2bkdn@gmail.com>
2017-03-15 01:35:07 +0700

    fix upload function

--- server/controllers/UploadController.js
+++ server/controllers/UploadController.js
@@ -6,18 +6,22 @@ const Util = require('../helpers/Util');
 const fs = require('fs');
 const path = require('path');
 const uuidV1 = require('uuid/v1');
+const formidable = require('formidable');
 
 module.exports = {
 	index(req, res) {
-		let fstream;
-		req.pipe(req.busboy);
-		req.busboy.on('file', function (fieldName, file, fileName, encoding, mimeType) {
-			let newFileName = `${uuidV1()}_${fileName}`;
-			fstream = fs.createWriteStream(path.resolve(__dirname, '..', '..', `uploads/${newFileName}`));
-			file.pipe(fstream);
-			fstream.on('close', function () {
-				return res.json(Util.success('Upload success!', newFileName));
-			});
-		});
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+        	let file = files.file;
+            let newFileName = `${uuidV1()}_${file.name}`;
+            var oldPath = file.path;
+            var newPath = path.resolve(__dirname, '..', '..', `uploads/${newFileName}`);
+            fs.rename(oldPath, newPath, function (error) {
+                if (error){
+                    return res.status(httpStatus.BAD_REQUEST).json(Util.error(error.message, httpStatus.BAD_REQUEST));
+				}
+                return res.json(Util.success('Upload success!', newFileName));
+            });
+        });
 	}
 };
